1b0065dbc7f343f0e14649cd91875df9a95df92c
PatilOjas <ojaspatil1901@gmail.com>
2021-06-14 17:57:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Messenger
+ A complete system for a messenger with web site and smartphone applications
---
+++ server.py
@@ -0,0 +1,4 @@
+import socket
+import threading
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
